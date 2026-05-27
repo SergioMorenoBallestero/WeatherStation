3cae8738eaa5c5358b5671d4a3a4effd82c0b0c4
hrkoe <100582747@alumnos.uc3m.es>
2026-05-27 12:37:38 +0200

    Changes to the main function to adapt to the new class hierarchy

--- WeatherStation/src/main.cpp
+++ WeatherStation/src/main.cpp
@@ -1,20 +1,35 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <DHT.h>
 #include "sensor.h"
 
-OneWire wire(2);
-Sensor sens = Sensor(&wire);
+#define WIRE_BUS_PIN 2
+#define HUM_SENS_PIN 4
+
+
+OneWire wire_bus(WIRE_BUS_PIN);
+
+TempSensor temp_sens(&wire_bus);
+HumSensor hum_sens(HUM_SENS_PIN,DHT11);
 
 void setup() {
   Serial.begin(9600);
-  sens.sensor.begin();
+  temp_sens.begin();
+  hum_sens.begin();
 }
 
 void loop() {
-  sens.updateReading();
-  Serial.print("Reading: ");
-  Serial.print(sens.getReading());
+  temp_sens.updateReading();
+  hum_sens.updateReading();
+  Serial.print("Temp reading: ");
+  Serial.print(temp_sens.getReading());
   Serial.print(" ºC\n");
+  temp_sens.printReadings();
+  Serial.print("Humidity reading: ");
+  Serial.print(hum_sens.getReading());
+  Serial.print(" %\n");
+  hum_sens.printReadings();
   delay(2000);
+  
 }
